7c6ec73222d88df9a3fca423a84c2f044a813241
Swam Htet Aung <swamhtetaungg@gmail.com>
2023-07-01 13:20:02 +0630

    fix hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,43 +4,42 @@ import { BsChevronDown } from "react-icons/bs";
 import SocialLinks from "./SocialLinks";
 const Hero = () => {
   return (
-    <section className=" w-full md:py-20 py-10">
-      <div className=" w-full flex md:flex-row flex-col justify-between gap-20">
-        <div className=" flex flex-col gap-10 items-start">
-          <h1 className="text-4xl font-bold capitalize text-gray-900">
+    <section className="w-full py-10 md:py-20">
+      <div className="flex flex-col justify-between w-full gap-20 md:flex-row">
+        <div className="flex flex-col items-start gap-10 ">
+          <h1 className="text-4xl font-bold text-gray-900 capitalize">
             I am Swam Htet Aung
             <br />
-            <span className=" my-3 border-b-4 pb-3 border-sky-500 text-lg text-gray-900">
+            <span className="pb-3 my-3 text-lg text-gray-900 border-b-4 border-sky-500">
               Web Developer
             </span>
           </h1>
-          <p className=" max-w-xl leading-8  text-black/70 ">
-            I have studied Computer Science for three years at the University of
-            Information Technology and developed a strong passion for software
-            engineering and web development. In addition to my academic
-            background, I have over three years of experience as a Japanese
-            lecturer, improving my communication, teaching, and work ethic
-            skills. Currently, I am pursuing full-stack web development and
-            working as an web developer intern to enhance my skills and achieve
-            my goal of becoming a successful web developer.
+          <p className="max-w-xl leading-8 text-black/70">
+            I studied Computer Science for three years, specializing in software
+            engineering and web development. With over three years of experience
+            as a Japanese lecturer, I honed my communication and teaching
+            skills. Now, I am pursuing full-stack web development and have
+            immersed myself in staying up-to-date with the latest technologies
+            while simultaneously contributing to the development of impactful
+            applications that benefit our society and environment.
           </p>
           <a
             download={true}
             href="/Resume.pdf"
-            className="group bg-gradient-to-r from-sky-500 text-white to-blue-500 py-3 px-8 rounded-md flex items-center gap-2">
+            className="flex items-center gap-2 px-8 py-3 text-white rounded-md group bg-gradient-to-r from-sky-500 to-blue-500">
             Download CV
-            <span className=" group-hover:translate-y-1 transition-all duration-200">
+            <span className="transition-all duration-200 group-hover:translate-y-1">
               <BsChevronDown size={18} />
             </span>
           </a>
         </div>
-        <div className=" divide-y space-y-5">
+        <div className="space-y-5 divide-y ">
           <Image
             src="/assets/images/profile.jpg"
             alt=""
             width={333}
             height={400}
-            className=" rounded-md object-cover mx-auto"
+            className="object-cover mx-auto rounded-md "
           />
           <SocialLinks />
         </div>
