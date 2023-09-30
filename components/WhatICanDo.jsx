380a7a329a8e67193c4969389f9840753648d7ce
Swam Htet Aung <swamhtetaungg@gmail.com>
2023-09-30 14:03:54 +0630

    Update resume and skills

--- components/WhatICanDo.jsx
+++ components/WhatICanDo.jsx
@@ -12,6 +12,7 @@ const WhatICanDo = () => {
         "react.svg",
         "nextjs.svg",
         "tailwind.svg",
+        "angular.png",
         "typescript.svg",
         "js.png",
         "bootstrap.png",
@@ -24,25 +25,25 @@ const WhatICanDo = () => {
       title: "Backend Web Development and Web Applications",
       details:
         "I possess the expertise to design and construct robust, scalable server applications that are capable of seamlessly handling increased traffic and user loads. My proficiency with cutting-edge technologies such as PHP, Laravel, and MySQL allows me to implement efficient and optimized solutions that can be easily maintained over time.",
-      techs: ["laravel.svg", "php.png", "mysql.svg"],
+      techs: ["laravel.svg", "php.png", "java.png", "mysql.svg"],
     },
   ];
   return (
-    <section className=" w-full md:py-20 py-10">
-      <h1 className="text-4xl text-gray-900 font-bold capitalize border-b-4 mb-10 border-sky-500 inline-block pb-3">
+    <section className="w-full py-10 md:py-20">
+      <h1 className="inline-block pb-3 mb-10 text-4xl font-bold text-gray-900 capitalize border-b-4 border-sky-500">
         What I can do
       </h1>
-      <div className="my-5 rounded space-y-10">
+      <div className="my-5 space-y-10 rounded">
         {services.map((service) => (
           <article
             key={service.id}
-            className="rounded-lg border border-gray-100 bg-white p-4 shadow-sm transition hover:shadow-lg sm:p-6 space-y-5">
+            className="p-4 space-y-5 transition bg-white border border-gray-100 rounded-lg shadow-sm hover:shadow-lg sm:p-6">
             <h3 className="mt-0.5 text-2xl font-medium text-gray-900">
               {service.title}
             </h3>
 
             <p className="mt-2 leading-8 text-gray-500">{service.details}</p>
-            <div className=" my-3 flex items-center gap-3 flex-wrap">
+            <div className="flex flex-wrap items-center gap-3 my-3 ">
               {service.techs.map((tech) => (
                 <Image
                   src={`/assets/images/skills/${tech}`}
