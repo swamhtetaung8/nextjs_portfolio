7361c5b8c00969f89db4e4f06c0ff63a47e12afd
Swam Htet Aung <swamhtetaungg@gmail.com>
2025-02-16 21:56:46 +0700

    feat:update resume google drive link

--- components/home/Hero.jsx
+++ components/home/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
 
           <div className="flex items-center gap-2">
             <a
-              href="https://drive.google.com/file/d/1_LgIgCsd_xQRCel1mwpeIo1Rt7Zix1YO/view?usp=sharing"
+              href="https://drive.google.com/file/d/1ayzLWNEssJ7hL8OUpRraUByg5JmvrwMY/view?usp=sharing"
               target="_blank"
               className="relative px-4 py-2 md:px-8 md:py-3 text-white transition-all duration-700 rounded-full group bg-gradient-to-r from-sky-500 to-blue-500">
               <span className="absolute inset-0 transition-opacity duration-500 rounded-full opacity-0 group-hover:opacity-100 bg-gradient-to-r from-sky-300 to-blue-700"></span>
