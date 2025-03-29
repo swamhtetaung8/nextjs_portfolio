e2918152c4a4459e4d733f3b28118462e892b3af
Swam Htet Aung <swamhtetaungg@gmail.com>
2025-03-29 11:12:37 +0700

    chore: move resume url into env

--- components/home/Hero.jsx
+++ components/home/Hero.jsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import React from "react";
 import SocialLinks from "./SocialLinks";
+
+const NEXT_PUBLIC_RESUME_URL = process.env.NEXT_PUBLIC_RESUME_URL
+
 const Hero = () => {
   return (
     <section className="relative w-full py-10">
@@ -23,7 +26,7 @@ const Hero = () => {
 
           <div className="flex items-center gap-2">
             <a
-              href="https://drive.google.com/file/d/1yL6dbETlVE-1MZVx2RrK9ovamegzNhPm/view?usp=sharing"
+              href={NEXT_PUBLIC_RESUME_URL}
               target="_blank"
               className="relative px-4 py-2 md:px-8 md:py-3 text-white transition-all duration-700 rounded-full group bg-gradient-to-r from-sky-500 to-blue-500">
               <span className="absolute inset-0 transition-opacity duration-500 rounded-full opacity-0 group-hover:opacity-100 bg-gradient-to-r from-sky-300 to-blue-700"></span>
